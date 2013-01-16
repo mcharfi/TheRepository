c013863dfd1deaa57e28d698c356d5db255ccebb
Mehdi <mcharfi@seas.upenn.edu>
2013-01-15 20:57:58 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MehdiApp::Application.config.secret_token = 'fe683be0af5ce2a507e106280512919757edcf9ffae1363427233953afa77a72a2c1286a988c9340cd644139118fd30ec030f22858e02919f231ad995e439973'
